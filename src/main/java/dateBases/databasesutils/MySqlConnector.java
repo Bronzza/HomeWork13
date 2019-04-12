c40ef10e69c3539015336a474a0d8a7c680c460c
Sergey Bronzza <repinetskiy@gmail.com>
2019-04-13 00:04:07 +0300

    Commint to SecondBranch InputStream instead of FileReader for use relative path

--- src/main/java/dateBases/databasesutils/MySqlConnector.java
+++ src/main/java/dateBases/databasesutils/MySqlConnector.java
@@ -4,9 +4,12 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import org.apache.log4j.Logger;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -16,18 +19,16 @@ import java.util.Properties;
 @NoArgsConstructor
 public class MySqlConnector {
     private static Properties properties = new Properties();
-    private static String propertiesPath = "C:\\Users\\Sergey\\Desktop\\api-samples-master\\HomeWork13\\src\\main\\" +
-            "resources\\dbConfig.properties";
-    private static FileReader fileReader;
-
+    private static String propertiesPath = "dbConfig.properties";
     private final static Logger LOGGER = Logger.getLogger(MySqlConnector.class);
-
     private static Connection connection;
 
     public static Connection getConnection() {
+
         try {
-            fileReader = new FileReader(propertiesPath);
-            properties.load(fileReader);
+            ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
+            InputStream propertiesStream = classLoader.getResourceAsStream(propertiesPath);
+            properties.load(propertiesStream);
             connection = DriverManager.getConnection(properties.getProperty("db.conn.url"),
                     properties.getProperty("db.username"), properties.getProperty("db.password"));
         } catch (SQLException | IOException e) {
